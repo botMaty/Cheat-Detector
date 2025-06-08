91d7b716da6a7e484170c924b03a640775695860
Mahdi <mhd.moalllem@gmail.com>
2025-06-08 16:01:58 +0330

    add quiz.html in templates and fix app.py (make a route for quiz)

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, session, url_for
 
 app = Flask(__name__)
+app.secret_key = "1234"
 
 @app.route("/")
 def home():
-    return render_template("login.html")
+    return redirect(url_for("login"))
+    
+@app.route("/login/", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        name = request.form["login_name"]
+        if len(name) > 2:
+            session["name"] = name
+            return redirect(url_for("login"))
+        else:
+            return render_template("login.html") # error
+    else:
+        if "name" in session:
+            session.pop("name")
+        return render_template("login.html")
+        
+@app.route("/quiz/")
+def quiz():
+    if "name" not in session:
+        return redirect(url_for("login"))
+    return render_template("quiz.html")
